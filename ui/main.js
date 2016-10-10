2a93d1db788f2de03992414c5e83bb313e982550
NikJoj <nikhiljoji07@gmail.com>
2016-10-10 23:03:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,26 @@
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function() {
      
-     //make a request to the counter
+     //creat request
+     var request= new XMLHttpRequest();
      
      //capture the response and store it in a variable
+     request.onreadystatechange=function() {
+         if (request.readyState===XMLHttpRequest.DONE) {
+             //take some action
+             if (request.status===200) {
+             var counter =request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML=counter.toString();
+                 
+             }
+         }
+     };
      
-     //render the variable in the correct span
-counter=counter+1;
-var span = document.getElementById('count');
-span.innerHTML=counter.toString();
+     request.open('GET', 'http://nikjoj.imad.hasura-app.io/counter',true);
+     requesr.send(null);
+
+
 };
