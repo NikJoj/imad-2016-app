69f499595639659d92bb122e82f0880cf782c826
NikJoj <nikhiljoji07@gmail.com>
2016-10-10 14:04:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element= document.getElementById('text');
 element.innerHTML='This is my first WebApp';
+
+var img= document.getElementById('pic');
+img.onclick=function() {
+img.style.marginLeft='100px';
+};
