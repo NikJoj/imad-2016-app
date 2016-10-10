f034ba1ef3a84f2851f5b4fd97b714b595a0e3a6
NikJoj <nikhiljoji07@gmail.com>
2016-10-10 22:54:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
-console.log('Loaded!');
-
-
-var marginLeft=0;
-function moveRight() {
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-var img= document.getElementById('pic');
-img.onclick=function() {
-    var interval = setInterval(moveRight,10);
+//counter code
+var button=document.getElementById('counter');
+button.onclick=function() {
+     
+     //make a request to the counter
+     
+     //capture the response and store it in a variable
+     
+     //render the variable in the correct span
+counter=counter+1;
+var span = document.getElementById('count');
+span.innerHTML=counter.toString();
 };
