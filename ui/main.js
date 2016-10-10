541c5cff4fafd37d2601bce84708bc8e99ab633e
NikJoj <nikhiljoji07@gmail.com>
2016-10-10 14:06:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,10 @@ element.innerHTML='This is my first WebApp';
 
 var marginLeft=0;
 function moveRight() {
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
 }
 var img= document.getElementById('pic');
 img.onclick=function() {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,10);
 };
